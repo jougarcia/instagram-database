6e1cf434fb510694a0186794e445e9450c509b6c
jougarcia <jougarcia95@gmail.com>
2022-12-14 03:06:39 +0000

    joseph

--- src/models.py
+++ src/models.py
@@ -8,52 +8,54 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
+class Follower(Base):
+    __tablename__='follower'
+    id = Column(Integer, primary_key=True)
+    from_id=Column(Integer,ForeignKey('user.id'))
+    From_user=relationship("User",backref='user')
+    to_id=Column(Integer,ForeignKey('user.id'))
+    to_user=relationship("User",backref='user')
+
 class User(Base):
     __tablename__ = 'user'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
-    email = Column(String(250), nullable=False)
-    fav_planets = relationship('Fav_planets', backref='user', lazy=True)
-    fav_characters = relationship('Fav_characters', backref='user', lazy=True)
-    
+    username = Column(String(250), nullable=False)
+    firstname = Column(String(250), nullable=False)
+    lastname = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False, unique=True)
+    comment = relationship('Comment', backref='user', lazy=True)
+    post = relationship('Post', backref='user', lazy=True)
+    follower = relationship('Follower', backref='user', lazy=True)
 
-class Planets(Base):
-    __tablename__ = 'planets'
+class Comment(Base):
+    __tablename__ = 'comment'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    planet_diameter = Column(Integer)
-    planet_terrain = Column(String(250))
-    planet_name = Column(String(250), nullable=False)
-    fav_planets = relationship('Fav_planets', backref='user', lazy=True)
+    comment_text = Column(String(250), nullable=False)
+    author_id = Column(Integer,ForeignKey('user.id'))
+    post_id = Column(Integer,ForeignKey('post.id'))
 
 
-class Characters(Base):
-    __tablename__ = 'characters'
+class Post(Base):
+    __tablename__ = 'post'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    characters_mass = Column(Integer)
-    characters_gender = Column(String(250))
-    characters_name = Column(String(250), nullable=False)
-    fav_characters = relationship('Fav_characters', backref='user', lazy=True)
+    user_id = Column(Integer,ForeignKey('user.id'))
 
-class Fav_planets(Base):
-    __tablename__ = 'fav_planets'
+class Media(Base):
+    __tablename__ = 'media'
     id = Column (Integer,primary_key=True)
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
-    user_id = Column(Integer,ForeignKey('user.id'))
-    planets_id = Column(Integer,ForeignKey('planets.id'))
+    type =Column(String(250))
+    url =Column(String(250))
+    post_id = Column(Integer,ForeignKey('post.id'))
 
     
-class Fav_characters(Base):
-    __tablename__ = 'fav_characters'
-    id = Column (Integer,primary_key=True)
-    user_id = Column(Integer,ForeignKey('user.id'))
-    charaters_id = Column(Integer,ForeignKey('characters.id'))
 
 
     def to_dict(self):
